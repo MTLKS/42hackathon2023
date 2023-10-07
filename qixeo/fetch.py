bf8435f2292e3d6e0d876aef8117a9747946aebe
HatsuneQixeo <qiyong831@gmail.com>
2023-10-07 15:07:49 +0000

    fetch script and some refactor

---
+++ qixeo/fetch.py
@@ -0,0 +1,36 @@
+from get_access_token import get_access_token, DOMAIN
+import sys
+import subprocess
+import json
+
+
+def quote_if_hasspace(string: str) -> str:
+    return f'"{string}"' if ' ' in string else string
+
+
+def main():
+    try:
+        argc = len(sys.argv)
+        if argc < 2:
+            raise RuntimeError(f"Usage: {sys.argv[0]} <uri> <...params>")
+        uri = sys.argv[1]
+        args = sys.argv[2:]
+        command = [
+            'curl',
+            '-H',
+            f'Authorization: Bearer {get_access_token()}',
+            f'{DOMAIN}/v2/{uri}?{"&".join(args)}'
+        ]
+
+        print("url:", command[-1])
+        child = subprocess.run(command, capture_output=True)
+        json.dump(json.loads(child.stdout), sys.stdout, indent='\t')
+    except KeyboardInterrupt:
+        exit(130)
+    except Exception as e:
+        print('Error:', e)
+        exit(1)
+
+
+if __name__ == "__main__":
+  main()
