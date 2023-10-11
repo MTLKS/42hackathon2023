64a03aed4c1468abf5e55656f44307c07b605a3c
HatsuneQixeo <qiyong831@gmail.com>
2023-10-11 10:57:51 +0000

    help command cleanup

--- src/commands/help.command.ts
+++ src/commands/help.command.ts
@@ -10,19 +10,30 @@ export class HelpCommand {
 	})
 	public async onHelp(@Context() [interaction]: SlashCommandContext) {
 
+    let commands = new Map<string, string>()
+
+    commands.set('**/ping**', '*Pong (for testing)*');
+    commands.set('**/login**', '*Login to 42 intra *');
+    commands.set('**/rusheval info**', '*Get current info about rush eval*');
+    commands.set('**/rusheval cadet**', '*Get cadets to create timeslots*');
+    commands.set('**/rusheval pisciners**', '*Get pisciners to choose timeslots*');
+    commands.set('**/rusheval match**', '*Lock in cadet and pisciner timeslots*');
+    commands.set('**/rusheval feedback**', '*Get feedback from rush evaluators*');
+    commands.set('**/updateroles**', '*Update user roles*');
     const newEmbed = new EmbedBuilder()
       .setColor('#00FFFF')
       .setTitle('__Available Commands__')
       .addFields(
-        { name: '__Commands__                                  __Descriptions__', value: '\n'},
-        { name: '`/ping`                                    :      Pong (for testing)', value: '\n'},
-        { name: "`/login`                                  :      Login to 42 intra ", value: '\n'},
-        { name: '`/rusheval info`               :      Get current info about rush eval', value: '\n'},
-        { name: '`/rusheval cadet`             :      Get cadets to create timeslots', value: '\n'},
-        { name: '`/rusheval pisciners`   :      Get pisciners to choose timeslots', value: '\n'},
-        { name: '`/rusheval match`            :      Lock in cadet and pisciner timeslots', value: '\n'},
-        { name: '`/rusheval feedback`      :      Get feedback from rush evaluators', value: '\n'},
-        { name: '`/updateroles`                   :      Update user roles', value: '\n'},
+        {
+          name: '__Commands__',
+          value: [...commands.keys()].join('\n\n'),
+          inline: true
+        },
+        {
+          name: '__Descriptions__',
+          value: [...commands.values()].join('\n\n'),
+          inline:true
+        }
       )
     return interaction.reply({ content: '', ephemeral: true, embeds: [newEmbed]});
   }
