a17989a1268f5ca8a729e6c56f980620e1c1fe53
Pei <nuyiep@hotmail.com>
2023-10-11 10:41:36 +0000

    rearranged help command order

--- src/commands/help.command.ts
+++ src/commands/help.command.ts
@@ -17,11 +17,11 @@ export class HelpCommand {
         { name: '__Commands__                                  __Descriptions__', value: '\n'},
         { name: '`/ping`                                    :      Pong (for testing)', value: '\n'},
         { name: "`/login`                                  :      Login to 42 intra ", value: '\n'},
-        { name: '`/rusheval cadet`             :      Get cadets to create timeslots', value: '\n'},
-        { name: '`/rusheval feedback`      :      Get feedback from rush evaluators', value: '\n'},
         { name: '`/rusheval info`               :      Get current info about rush eval', value: '\n'},
-        { name: '`/rusheval match`            :      Lock in cadet and pisciner timeslots', value: '\n'},
+        { name: '`/rusheval cadet`             :      Get cadets to create timeslots', value: '\n'},
         { name: '`/rusheval pisciners`   :      Get pisciners to choose timeslots', value: '\n'},
+        { name: '`/rusheval match`            :      Lock in cadet and pisciner timeslots', value: '\n'},
+        { name: '`/rusheval feedback`      :      Get feedback from rush evaluators', value: '\n'},
         { name: '`/updateroles`                   :      Update user roles', value: '\n'},
       )
     return interaction.reply({ content: '', ephemeral: true, embeds: [newEmbed]});
