85f4b8056b00c4f28d7ac3016102dad84359afd9
HatsuneQixeo <qiyong831@gmail.com>
2023-10-11 13:14:03 +0000

    updateroles command cleanup and synced

--- src/commands/updateroles.command.ts
+++ src/commands/updateroles.command.ts
@@ -3,6 +3,7 @@ import { SlashCommand, SlashCommandContext, Context } from 'necord';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Student } from '../schema/student.schema';
+import { Role } from 'discord.js';
 
 @Injectable()
 export class UpdateRolesCommand {
@@ -16,50 +17,51 @@ export class UpdateRolesCommand {
     defaultMemberPermissions: ['Administrator']
   })
   public async onExecute(@Context() [interaction]: SlashCommandContext) {
-    const getRole = (role: string) => interaction.guild.roles.cache.find((r) => r.name === role);
-    const pisciner = getRole('PISCINER');
-    const floaty = getRole('FLOATY');
-    const cadet = getRole('CADET');
-    const specialization = getRole('SPECIALIZATION');
-
-    const ss = interaction.guild.roles.cache.find((r) => r.name === 'Segmentation Slayers');
-    const bb = interaction.guild.roles.cache.find((r) => r.name === 'Bug Busters');
-    const kk = interaction.guild.roles.cache.find((r) => r.name === 'Kernel Kamikazes');
-    const uu = interaction.guild.roles.cache.find((r) => r.name === 'Unix Unicorns');
+    const getRole = (role: string) =>
+      interaction.guild.roles.cache.find((r) => r.name === role);
+    const role_map = new Map<string, Role>([
+        'PISCINER',
+        'FLOATY',
+        'CADET',
+        'SPECIALIZATION'
+      ].map(value => [value, getRole(value)])
+    );
+    const coalition_map = new Map<string, Role>([
+        'Segmentation Slayers',
+        'Bug Busters',
+        'Kernel Kamikazes',
+        'Unix Unicorns'
+      ].map(value => [value, getRole(value)])
+    );
+    const everyRoles = [...role_map.values(), ...coalition_map.values()];
 
     await interaction.deferReply({ ephemeral: true });
+    const members = await interaction.guild.members.fetch();
+    const getMemberRoles = (student: Student) => {
+      let roles_add: Role[] = [];
 
-    interaction.guild.members.fetch().then(members => {
-      members.forEach(async(member) => {
-        const student = await this.studentModel.findOne({ discordId: member.id });
-        if (student != null) {
-          await member.roles.remove([pisciner, floaty, cadet, specialization, ss, bb, kk, uu])
-            .then(async() => {
-              if (student.progressRole == 'PISCINER') {
-                await member.roles.add(pisciner);
-              } else if (student.progressRole == 'FLOATY') {
-                await member.roles.add(floaty);
-              } else if (student.progressRole == 'CADET') {
-                await member.roles.add(cadet);
-              } else if (student.progressRole == 'SPECIALIZATION') {
-                await member.roles.add([cadet, specialization]);
-              }
-              if (student.coalitionRole == 'Segmentation Slayers') {
-                await member.roles.add(ss);
-              } else if (student.coalitionRole == 'Bug Busters') {
-                await member.roles.add(bb);
-              } else if (student.coalitionRole == 'Kernel Kamikazes') {
-                await member.roles.add(kk);
-              } else if (student.coalitionRole == 'Unix Unicorns') {
-                await member.roles.add(uu);
-              }
-            });
-        }
-      })
-      return interaction.editReply({ content: 'All member roles have been updated!' });
-    })
-    .catch(err => {
-      return interaction.editReply({ content: 'Something went wrong!' });
-    });
+      if (student.progressRole === 'SPECIALIZATION') {
+        roles_add.push(role_map.get('CADET'));
+      } 
+      if (role_map.has(student.progressRole)) {
+        roles_add.push(role_map.get(student.progressRole));
+      }
+      if (coalition_map.has(student.coalitionRole)) {
+        roles_add.push(coalition_map.get(student.coalitionRole));
+      }
+      return roles_add;
+    };
+
+    for (let [id, member] of members) {
+      const student = await this.studentModel.findOne({ discordId: id });
+      if (student === null)
+        continue ;
+      const roles = getMemberRoles(student);
+
+      // interaction.editReply({ content: `Assigning ${member} as ${roles}` });
+      await member.roles.remove(everyRoles);
+      await member.roles.add(roles);
+    }
+    return interaction.editReply({ content: 'All member roles have been updated!' });
   }
 }
