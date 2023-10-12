e4cf214560ac08d0047ffe09d00652aff5db04c4
HatsuneQixeo <qiyong831@gmail.com>
2023-10-12 01:46:24 +0000

    fill command team generation

--- src/commands/fill.command.ts
+++ src/commands/fill.command.ts
@@ -7,6 +7,7 @@ import { Model } from "mongoose";
 import { Context, SlashCommand, SlashCommandContext, Subcommand } from "necord";
 import { Evaluator } from "src/schema/evaluator.schema";
 import { Student } from "src/schema/student.schema";
+import { Team } from "src/schema/team.schema";
 import { Timeslot } from "src/schema/timeslot.schema";
 
 type ProgressRole = 'CADET' | 'PISCINER';
@@ -40,7 +41,7 @@ class GarbageGenerator {
     return student;
   }
 
-  public static randEvaluator(slots: Timeslot[] = this.randTimeslots(randomInt(6))) {
+  public static randEvaluator(slots: Timeslot[] = this.randTimeslots(randomInt(1, 6))) {
     const evaluator: Evaluator = {
       student: this.randStudent('CADET'),
       timeslots: slots
@@ -67,7 +68,7 @@ class GarbageGenerator {
       res.push(set[i]);
       set.splice(i, 1);
     }
-    const compare_time = (a: string, b: string) => {
+    const compareTime = (a: string, b: string) => {
       const toInt = (time: string) => {
         const n = parseInt(time.substring(0, time.indexOf(':')));
 
@@ -76,7 +77,15 @@ class GarbageGenerator {
 
       return toInt(a) - toInt(b);
     };
-    return res.sort(compare_time).map(slot => {return {timeslot: slot}});
+    return res.sort(compareTime).map(slot => {return {timeslot: slot}});
+  }
+
+  public static randTeam(evaluatorTimeslots: Timeslot[]) {
+    return {
+      teamLeader: this.randStudent(),
+      teamMembers: [this.randStudent(), this.randStudent()],
+      timeslot: evaluatorTimeslots[randomInt(evaluatorTimeslots.length)]
+    };
   }
 }
 
@@ -84,7 +93,7 @@ class GarbageGenerator {
 export class FillGarbageCommand {
   constructor(
     @InjectModel(Evaluator.name) private readonly evaluatorModel: Model<Evaluator>,
-    @InjectModel(Student.name) private readonly studentModel: Model<Student>,
+    @InjectModel(Team.name) private readonly teamModel: Model<Team>,
   ) {}
 
   @SlashCommand({
@@ -92,26 +101,31 @@ export class FillGarbageCommand {
     description: 'generate and insert a fake data for experimental purpose'
   })
   public async onPing(@Context() [interaction]: SlashCommandContext) {
-    const student = GarbageGenerator.randEvaluator();
+    const evaluator = GarbageGenerator.randEvaluator();
+    const team = GarbageGenerator.randTeam(evaluator.timeslots);
 
-    this.evaluatorModel.insertMany([student]);
+    this.evaluatorModel.insertMany([evaluator]);
+    this.teamModel.insertMany([team]);
     const embed = new EmbedBuilder()
       .addFields({
-          name: student.student.intraName,
+          name: evaluator.student.intraName,
           value: ['intraId', 'discordId', 'progressRole', 'coalitionRole', 'timeslots'].map(s => `**${s}**`).join('\n'),
           inline: true
-        },
-        {
+        }, {
           name: 'data',
           value: [
-            student.student.intraId,
-            student.student.discordId,
-            student.student.progressRole,
-            student.student.coalitionRole,
-            student.timeslots.map(s => s.timeslot).join(', ') || 'N/A'
+            evaluator.student.intraId,
+            evaluator.student.discordId,
+            evaluator.student.progressRole,
+            evaluator.student.coalitionRole,
+            evaluator.timeslots.map(s => s.timeslot).join(', ') || 'N/A'
           ].join('\n'),
           inline: true
-        })
+        }, {
+          name: `${team.teamLeader.intraName}'s group`,
+          value: team.teamMembers.map(m => m.intraName).join('\n')
+        }
+      )
     ;
     return interaction.reply({
         embeds: [embed],
