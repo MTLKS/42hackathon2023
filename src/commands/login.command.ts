3635d6f13c336d083fa2cc4b5d9ebcad62ee4600
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2023-12-26 20:53:43 +0800

    LoginCommand error message and cleanup

--- src/commands/login.command.ts
+++ src/commands/login.command.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
 import { SlashCommand, SlashCommandContext, Context } from 'necord';
-import { Embed, EmbedBuilder, embedLength } from 'discord.js';
+import { EmbedBuilder } from 'discord.js';
 
 @Injectable()
 export class LoginCommand {
@@ -10,31 +10,37 @@ export class LoginCommand {
     description: 'Login to 42 intra',
   })
   public async onLogin(@Context() [interaction]: SlashCommandContext) {
-    let url = '';
-    if (process.env.PORT == undefined) {
-      url = `${process.env.HOST}/login/${interaction.user.id}`;
-    } else {
-      url = `${process.env.HOST}:${process.env.PORT}/login/${interaction.user.id}`;
+    const port = (process.env.PORT !== undefined) ? `:${process.env.PORT}`: "";
+    const url = `${process.env.HOST}${port}/login/${interaction.user.id}`
+
+    try {
+      const newEmbed = new EmbedBuilder()
+        .setColor('#00FFFF')
+        .setTitle('Login to 42 intra')
+        .setDescription('Click on the button below to login to 42 intra')
+        .setURL(url)
+        ;
+
+      const button = new ButtonBuilder()
+        .setStyle(ButtonStyle.Link)
+        .setLabel('Login')
+        .setURL(url)
+        ;
+      
+      const row = new ActionRowBuilder<ButtonBuilder>()
+        .addComponents(button);
+
+      return interaction.reply({
+          ephemeral: true,
+          embeds: [newEmbed],
+          components: [row]
+        });
+    } catch (error) {
+      console.error(error);
+      return interaction.reply({
+        ephemeral: true,
+        content: 'An error occured while trying to login to 42 intra'
+      });
     }
-    
-    const newEmbed = new EmbedBuilder()
-      .setColor('#00FFFF')
-      .setTitle('Login to 42 intra')
-      .setDescription('Click on the button below to login to 42 intra')
-      .setURL(url)
-    ;
-
-    const button = new ButtonBuilder()
-      .setStyle(ButtonStyle.Link)
-      .setLabel('Login')
-      .setURL(url);
-    
-    const row = new ActionRowBuilder<ButtonBuilder>()
-      .addComponents(button);
-
-    return interaction.reply({
-      ephemeral: true,
-      embeds: [newEmbed],
-      components: [row] });
   }
 }
