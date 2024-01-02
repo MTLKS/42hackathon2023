b27b7d96fbf176f2471ba6d931f85e78640512df
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2024-01-03 02:54:36 +0800

    ApiManager.getProjectUsers, Project status enum, and some cleanup

--- src/ApiManager.ts
+++ src/ApiManager.ts
@@ -1,8 +1,17 @@
 import { HttpService } from "@nestjs/axios";
-import { Logger } from "@nestjs/common";
+import { ConsoleLogger, Logger } from "@nestjs/common";
 import { firstValueFrom } from "rxjs";
 
+export enum ProjectStatus {
+  Finished = 'finished',
+  WaitingForCorrection = 'waiting_for_correction',
+  InProgress = 'in_progress',
+  SearchingAGroup = 'searching_a_group',
+  CreatingGroup = 'creating_group'
+}
+
 export class ApiManager {
+  private static readonly CAMPUS_ID = 34; // 42 Kuala Lumpur
   private static accessToken: string;
   private static readonly httpService = new HttpService();
   private static readonly logger = new Logger(ApiManager.name);
@@ -33,17 +42,40 @@ export class ApiManager {
   }
 
   public static getUser(intraIdOrLogin: string) {
-    return this.get42Api(`https://api.intra.42.fr/v2/users/${intraIdOrLogin}`);
+    return this.get42Api(`users/${intraIdOrLogin}`);
   }
 
   public static async getCoalitionRole(intraIdOrLogin: string): Promise<string> {
-    const response = await this.get42Api(`https://api.intra.42.fr/v2/users/${intraIdOrLogin}/coalitions`);
+    const response = await this.get42Api(`users/${intraIdOrLogin}/coalitions`);
     return response.data[0].name;
   }
 
-  /* TODO: This kinda don't belong to this class */
-  public static getDiscordUserData(id: string) {
-    return this.getDiscordApi(`https://discord.com/api/v10/users/${id}`);
+  public static async getProjectUsers(projectSlugOrId: string | number, intraId?: number, status?: ProjectStatus, page: number = 0): Promise<Array<any>> {
+    let queryArr: string[] = [];
+
+    if (intraId !== undefined) {
+      queryArr.push(`filter[user_id]=${intraId}`);
+    } else {
+      if (status !== undefined) {
+        queryArr.push(`filter[status]=${status}`);
+      }
+      queryArr.concat([
+        `filter[campus_id]=${this.CAMPUS_ID}`,
+        `page[number]=${page}`,
+        `page[size]=100`,
+      ]);
+    }
+    const query = `?${queryArr.join('&')}`;
+    try {
+      return await this.get42Api(`projects/${projectSlugOrId}/projects_users${query}`);
+    } catch (error) {
+      console.error(error);
+      const logger = new ConsoleLogger("ApiManager");
+    }
+  }
+
+  public static async getTeam(teamId: number) {
+    return this.get42Api(`teams/${teamId}`);
   }
 
   public static async get42Api(url: string) {
@@ -55,7 +87,12 @@ export class ApiManager {
         return ;
       }
     }
-    return await this.get(url, `Bearer ${this.accessToken}`);
+    return await this.get("https://api.intra.42.fr/v2/" + url, `Bearer ${this.accessToken}`);
+  }
+
+  /* TODO: This kinda don't belong to this class */
+  public static getDiscordUserData(id: string) {
+    return this.getDiscordApi(`https://discord.com/api/v10/users/${id}`);
   }
 
   private static async get(url: string, authorization: string) {
