73dcad6cd4d17f5533abf51b8324f6273aeb41ab
HatsuneQixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2024-01-05 18:44:41 +0800

    matching algorithm

--- src/commands/rusheval/match.command.ts
+++ src/commands/rusheval/match.command.ts
@@ -10,10 +10,11 @@ import { APIEmbedField, EmbedBuilder } from 'discord.js';
 import { getRole } from '../updateroles.command';
 import { ApiManager, ProjectStatus } from 'src/ApiManager';
 import { Student } from 'src/schema/student.schema';
+import { randomInt } from 'crypto';
 
 function getMissingEvaluatorEmbeds(teamsWithoutEvaluator: Team[]) {
-  const teamsNoRegister = teamsWithoutEvaluator.filter(team => !team.timeslot);
-  const teamsRegitered = teamsWithoutEvaluator.filter(team => team.timeslot);
+  const teamsNoRegister = teamsWithoutEvaluator.filter(team => team.timeslot === undefined);
+  const teamsRegitered = teamsWithoutEvaluator.filter(team => team.timeslot !== undefined);
   let fields: APIEmbedField[] = [];
 
   if (teamsNoRegister.length) {
@@ -49,25 +50,53 @@ export class RushEvalMatchCommand {
     @InjectModel(Evaluator.name) private readonly evaluatorModel: Model<Evaluator>,
   ) {}
 
-  private async matching() {
+  private async matching(rushProjectSlug: string) {
     let teams = await this.teamModel.find({ timeslot: {$ne: undefined}}).exec();
     let evaluators = await this.evaluatorModel.find().exec();
 
-    for (let t of teams) {
-      for (let e of evaluators) {
-        const matchedSlot = e.timeslots.find(slot =>
-          slot.timeslot === t.timeslot.timeslot);
-
-        if (!matchedSlot) {
-          continue ;
-        }
-        t.timeslot = matchedSlot;
-        t.evaluator = e.student;
-        e.timeslots.splice(e.timeslots.indexOf(matchedSlot), 1);
-        await t.save();
-        break ;
-      }
+    /* wanna sort the evaluator by their pool year */
+    if (rushProjectSlug === 'c-piscine-rush-00') {
+      evaluators.sort((a, b) => +b.student.poolYear - +a.student.poolYear);
+    } else if (rushProjectSlug === 'c-piscine-rush-01') {
+      evaluators.sort((a, b) => +a.student.poolYear - +b.student.poolYear);
+    } else if (rushProjectSlug === 'c-piscine-rush-02') {
+      throw new Error(`Not implemented yet: ${rushProjectSlug}`);
+    } else {
+      throw new Error(`Unknown rush project slug: ${rushProjectSlug}`);
     }
+
+    // const logEvaluator = (evaluator: Evaluator) => {
+    //   const student = evaluator.student;
+    //   return {
+    //     name: student.intraName,
+    //     poolYear: student.poolYear,
+    //     poolMonth: student.poolMonth,
+    //     timeslots: evaluator.timeslots.map(slot => slot.timeslot),
+    //   };
+    // };
+    // console.log("evaluators: ", evaluators.map(logEvaluator));
+    // console.log("teams: ", teams.map(t => t.timeslot.timeslot));
+    const matchPromises = teams.map(t => {
+      // if (t.evaluator !== undefined) {
+      //   continue ;
+      // }
+      const evaluatorsWithMatchingTimelot = evaluators.filter(e => e.timeslots.find(slot => slot.timeslot === t.timeslot.timeslot));
+
+      // console.log("evaluatorsWithMatchingTimelot: ", evaluatorsWithMatchingTimelot.map(logEvaluator));
+      const len = evaluatorsWithMatchingTimelot.length;
+      if (len === 0) {
+        return ;
+      }
+
+      /* prefer the first 50% of sorted evaluators */
+      const matchedEvaluator = evaluatorsWithMatchingTimelot[randomInt(Math.round(len / 2))];
+      const slot = matchedEvaluator.timeslots.find(slot => slot.timeslot === t.timeslot.timeslot);
+      t.timeslot = slot;
+      t.evaluator = matchedEvaluator.student;
+      matchedEvaluator.timeslots.splice(matchedEvaluator.timeslots.indexOf(slot), 1);
+      return t.save();
+    });
+    await Promise.all(matchPromises);
   }
 
   private async fetchOngoingRush(projectSlugOrId: string | number) {
@@ -100,7 +129,7 @@ export class RushEvalMatchCommand {
       replyContent += `This attempt will be assumed as testing since there is no ongoing \`\`${projectSlug}\`\` team that is waiting for correction.\n`;
     });
     try {
-      await this.matching();
+      await this.matching(projectSlug);
     } catch (error) {
       console.error(error);
       replyContent += `Error occured while matching teams and evaluators\n`;
@@ -137,12 +166,13 @@ export class RushEvalMatchCommand {
       // }).then(matchedEvaluatorsDc => {
         child.on('exit', (code, signal) => {
           if (code === 0) {
-            interaction.deleteReply();
+            // interaction.deleteReply();
             /** Ideal way is to assign a role for rush evaluators.
              * As I heard that there's problem with explicit individual ping.
              */
             replyContent += `Rush evaluation time table for dear evaluators: ${getRole(interaction.guild, 'CADET')}\n`;
-            return interaction.channel.send({content: replyContent, files: [outfile]})
+            // return interaction.channel.send({content: replyContent, files: [outfile]})
+            return interaction.editReply({content: replyContent, files: [outfile]})
               .finally(() => unlink(outfile, ()=>{}));
           } else {
             replyContent += `Error occured while generating time table\n`;
