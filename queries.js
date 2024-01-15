8f7fdee921b60ba2a81b29f71f3482dba6b4196b
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2024-01-16 02:55:44 +0800

    mongosh queries function

---
+++ queries.js
@@ -0,0 +1,37 @@
+db = connect('mongodb://localhost:27017/nest')
+
+/* This script is meant to be loaded in mongosh ( load("queries.js") ), providing the function to aggregate data */
+
+// show the teams sizes
+function countTeamSizes() {
+  const teams = db.teams.aggregate([
+    { $project: { _id: 0, name: 1, teamLeader: 1, teamMembers: 1 } },
+  ]);
+  const teamMembers = teams.map(team => ({ name: team.name, len: team.teamMembers.length + 1 }));
+
+  return teamMembers;
+}
+
+// group and show team by feedbacked and no feedback
+function showFeedbackGroup() {
+  const aggregation = (matchFeedback) => [
+    { $match: { feedback: matchFeedback } },
+    { $project: { _id: 0, name: 1, evaluator: "$evaluator.intraName", timeslot: "$timeslot.timeslot", feedbackAt: "$feedbackAt" } },
+    { $group: { _id: "$evaluator", teams: { $push: { name: "$name", timeslot: "$timeslot", feedbackAt: "$feedbackAt" } } } },
+    { $project: { evaluator: "$_id", teams: "$teams", _id: 0 } },
+  ];
+  const hasFeedback = db.teams.aggregate(aggregation({ $ne: undefined }));
+  const noFeedback = db.teams.aggregate(aggregation(undefined));
+
+  return { hasFeedback: hasFeedback.toArray(), noFeedback: noFeedback.toArray() };
+}
+
+// show teams that has a member to chose the timeslot instead of leader
+function showNonLeaderTimeslot() {
+  const feedback = db.teams.aggregate([
+    { $match: { $expr: { $ne: ["$teamLeader", "$chosenTimeslotBy"] } } },
+    { $project: { _id: 0, name: 1, evaluator: "$evaluator.intraName", timeslot: "$timeslot.timeslot", feedbackAt: 1, chosenTimeslotBy: "$chosenTimeslotBy.intraName" } },
+  ]);
+
+  return feedback;
+}
