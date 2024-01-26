c3712696dcb73bd37d74550de6b028aa2fb02419
Hatsune Qixeo <112535792+HatsuneQixeo@users.noreply.github.com>
2024-01-26 22:01:47 +0800

    fix export name and utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,18 @@
+export function monthNameToNumber(monthName: string) {
+  const monthNames = [
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
+
+  return monthNames.indexOf(monthName.toLowerCase()) + 1;
+}
